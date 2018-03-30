0d67d65e2189998bbefb2ec3d1144b26ba35b3fa
abhay9414 <abhay4079@gmail.com>
2018-03-30 09:40:26 -0700

    2

--- 3.c
+++ 3.c
@@ -1,4 +1,26 @@
 #include<stdio.h>
+#include<pthread.h>
+float CPU_UTIL()
+{
+    long double a[4], b[4], loadavg;
+    FILE *fp;
+    char dump[50];
+
+    fp = fopen("/proc/stat","r");
+        fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
+        fclose(fp);
+        sleep(1);
+
+        fp = fopen("/proc/stat","r");
+        fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
+        fclose(fp);
+
+        loadavg = ((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]))*100;
+        //printf("The current CPU utilization is : %Lf\n",loadavg);
+
+        return loadavg;
+}
+
 int main()
 {
   int remain,flag=0,ts;
